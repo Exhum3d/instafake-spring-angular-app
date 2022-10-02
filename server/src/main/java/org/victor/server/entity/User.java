4e01468dcae84235f19a2e3f2f0306791ba79be3
Exhum3d <sterea.victor@gmail.com>
2022-10-02 19:55:54 +0300

    Created the User entity

---
+++ server/src/main/java/org/victor/server/entity/User.java
@@ -0,0 +1,50 @@
+package org.victor.server.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 64, nullable = false)
+    private String firstName;
+
+    @Column(length = 64, nullable = false)
+    private String lastName;
+
+    @Column(length = 64, nullable = false)
+    private String email;
+
+    @Column(length = 256, nullable = false)
+    @JsonIgnore
+    private String password;
+
+    @Column(length = 256)
+    private String profileImage;
+
+    @Column(length = 32, nullable = false)
+    private String Role;
+
+    private Boolean accountVerified;
+
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private Date createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private Date updatedAt;
+}
