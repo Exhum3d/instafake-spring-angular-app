2a0344b433fb0d0db4e845b0c315f99a50821949
Exhum3d <sterea.victor@gmail.com>
2022-10-02 20:38:43 +0300

    Added Post entity relationships

--- server/src/main/java/org/victor/server/entity/Post.java
+++ server/src/main/java/org/victor/server/entity/Post.java
@@ -1,10 +1,13 @@
 package org.victor.server.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Data
@@ -32,4 +35,22 @@ public class Post {
 
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
     private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    @JsonIgnore
+    private List<Comment> comments = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "post_likes",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    @JsonIgnore
+    private List<User> likeList = new ArrayList<>();
+
 }
